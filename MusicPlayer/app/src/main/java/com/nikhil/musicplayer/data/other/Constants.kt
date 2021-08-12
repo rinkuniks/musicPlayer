6e96f360e276fedc1511b00b67ef9d1edf9d2f0b
Nikhil Kumar Singh <myemail>
2021-08-12 18:58:22 +0530

    ViewModel Implemented

--- MusicPlayer/app/src/main/java/com/nikhil/musicplayer/data/other/Constants.kt
+++ MusicPlayer/app/src/main/java/com/nikhil/musicplayer/data/other/Constants.kt
@@ -2,6 +2,8 @@ package com.nikhil.musicplayer.data.other
 
 object Constants {
     const val SONG_COLLECTION = "songs"
+    const val MEDIA_ROOT_ID = "root_id"
     const val NOTIFICATION_CHANNEL_ID = "music"
     const val NOTIFICATION_ID = 1
+    const val NETWORK_ERROR = "network_error"
 }
--- MusicPlayer/app/src/main/java/com/nikhil/musicplayer/di/AppModule.kt
+++ MusicPlayer/app/src/main/java/com/nikhil/musicplayer/di/AppModule.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.nikhil.musicplayer.R
+import com.nikhil.musicplayer.exoplayer.MusicServiceConnection
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,6 +16,11 @@ import javax.inject.Singleton
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun providesMusicServiceConnection(@ApplicationContext context: Context) =
+        MusicServiceConnection(context)
 
     @Provides
     @Singleton
