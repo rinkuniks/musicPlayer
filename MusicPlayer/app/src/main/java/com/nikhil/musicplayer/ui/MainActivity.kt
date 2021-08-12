5ed262db6201074e7d4c0b849ca09331cbbd01de
Nikhil Kumar Singh <myemail>
2021-08-12 19:03:26 +0530

    Change

--- MusicPlayer/app/src/main/java/com/nikhil/musicplayer/ui/MainActivity.kt
+++ MusicPlayer/app/src/main/java/com/nikhil/musicplayer/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.nikhil.musicplayer
+package com.nikhil.musicplayer.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.RequestManager
+import com.nikhil.musicplayer.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
